287804df5582be92f882c80425764037c164b024
Naveenkumar <naveen.sekaran01@gmail.com>
2025-12-27 14:39:49 +0530

    FUNCTIONAL INTERFACE AND STREAM API

---
+++ src/main/java/com/springboot/learning/core/FunctioinalInterface.java
@@ -0,0 +1,35 @@
+package com.springboot.learning.core;
+
+interface Animal{
+    public void behavior();
+}
+
+class Dog implements Animal{
+    public void behavior(){
+        System.out.println("Bark");
+    }
+}
+
+class Cat implements Animal{
+
+    public void behavior(){
+        System.out.println("Meeooww");
+    }
+}
+
+public class FunctioinalInterface {
+    public static void main(String[] args){
+      Animal animal = new Animal() {
+          public void behavior() {
+              System.out.println("Run");
+          }
+      };
+
+      Animal animal1 = ()-> System.out.println("Naveen");
+
+      animal.behavior();
+      animal1.behavior();
+
+
+    }
+}
