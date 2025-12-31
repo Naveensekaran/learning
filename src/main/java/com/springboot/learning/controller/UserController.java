feb99476f19ed36c5fa646eaee9c0b98659fea1c
Naveensekaran <naveen.sekaran01@gmail.com>
2025-12-31 11:37:55 +0530

    COMMANDLINE RUNNER & APPLICATION RUNNER COMPLETED

--- src/main/java/com/springboot/learning/controller/UserController.java
+++ src/main/java/com/springboot/learning/controller/UserController.java
@@ -1,8 +1,7 @@
 package com.springboot.learning.controller;
 
 import com.springboot.learning.service.UserService;
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/springboot/learning/service/UserService.java
+++ src/main/java/com/springboot/learning/service/UserService.java
@@ -3,6 +3,9 @@ package com.springboot.learning.service;
 import com.springboot.learning.model.UserDetails;
 import com.springboot.learning.repostiory.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +13,7 @@ import java.util.Optional;
 
 @Service
 @Profile("dev")
-public class UserService {
+public class UserService implements ApplicationRunner, CommandLineRunner {
 
     @Autowired
     private UserRepository userRepository;
@@ -18,5 +21,15 @@ public class UserService {
     public String getUserByUserId(String userId){
        Optional<UserDetails> user =  userRepository.findByUserId(userId);
        return user.map(UserDetails::getFirstName).orElse("User not found");
+    }
+
+    @Override
+    public void run(ApplicationArguments args){
+        System.out.println("Application started - Application Runner");
+    }
+
+    @Override
+    public void run(String... args){
+        System.out.println("Application started - Command Line Runner");
     }
 }
