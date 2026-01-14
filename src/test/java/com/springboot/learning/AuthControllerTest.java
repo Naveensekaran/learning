1c50a17f35a72ca0cfdb9c4e5f642e8c15ce67ce
Naveensekaran <naveen.sekaran01@gmail.com>
2026-01-14 18:23:15 +0530

    JUNIT COMPLETED

--- src/test/java/com/springboot/learning/AuthControllerTest.java
+++ src/test/java/com/springboot/learning/AuthControllerTest.java
@@ -4,10 +4,12 @@ import com.springboot.learning.controller.AuthController;
 import com.springboot.learning.model.Users;
 import com.springboot.learning.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
